fbf8315cfcef0ae1d8930ccfeef1a2a3fc071941
srinivasrao <srinivasrao452@gmail.com>
2022-05-27 08:16:45 +0530

    Creating Product models

--- Product_App/models.py
+++ Product_App/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+class Product(models.Model):
+    name = models.CharField(max_length=50, db_column="product_name")
+    price = models.FloatField(db_column="product_price")
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "products"
+
+
